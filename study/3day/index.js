412389803dbdaad6755a8aac9d4f88622d286f30
hanry <itsenxx99@gmail.com>
2024-01-23 11:46:35 +0900

    study

---
+++ study/3day/index.js
@@ -0,0 +1,7 @@
+'use script'
+document.querySelectorAll('.category-button').forEach(button => {
+  button.addEventListener("click", event => {
+    document.querySelector('#active-category-button').removeAttribute('id')
+    button.setAttribute('id', 'active-category-button');
+  })
+})
