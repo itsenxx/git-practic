1eaf1e2e505cc5979c3b085747904156458b438d
hanry <itsenxx99@gmail.com>
2024-01-24 10:50:18 +0900

    2024-01-24

--- study/4day/js/main.js
+++ study/4day/js/main.js
@@ -1,1 +1,47 @@
+/*
 // vanilla js
+const searchEl = document.querySelector('.search');
+const searchInputEl = document.querySelector('input');
+
+// input과 돋보기 아이콘을 클릭했을 때
+searchEl.addEventListener('click', function() {
+  searchInputEl.focus();
+});
+// inputbox 포커스가 될 때
+searchInputEl.addEventListener('focus', function() {
+  searchInputEl.setAttribute('placeholder', '통합검색');
+});
+// inputbox 포커스를 잃을 때
+searchInputEl.addEventListener('blur', function() {
+  searchInputEl.setAttribute('placeholder', '');
+});
+*/
+
+// jquery js
+// input과 돋보기 아이콘을 클릭했을 때
+$('.search').click(function() {
+  $('input').focus();
+});
+// inputbox 포커스가 될 때
+$('input').focus(function() {
+  $('input').attr('placeholder', '통합검색');
+});
+// inputbox 포커스를 잃을 때
+$('input').blur(function() {
+  $('input').attr('placeholder', '');
+});
+
+window.addEventListener('scroll', _.throttle(function() {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    //일정 스크롤 위치까지 내리면 배지 숨기기
+    gsap.to('header .badges', 0.4, {
+      opacity: 0
+    });
+  } else {
+    gsap.to('header .badges', 0.4, {
+      opacity: 1
+    });
+  }
+},300));
+
